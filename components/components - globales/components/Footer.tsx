8bb6423e9ffc27982465a7d1cd7b69d9496c71a9
UrpirioDev <urpiriojunior@gmail.com>
2026-01-01 12:24:15 -0400

    Se agrego un footer temporal

--- components/components - globales/components/Footer.tsx
+++ components/components - globales/components/Footer.tsx
@@ -1,4 +1,16 @@
-
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className=" w-full flex flex-col items-center justify-center py-20 border">
+      <div className="text-center w-full max-w-7xl">
+        <h3 className="text-2xl font-semibold">Flow-Ready</h3>
+        <p >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          molestias, corrupti nostrum facilis eveniet eum delectus consequatur
+          sunt perspiciatis? Accusamus ipsam sit ab ea numquam harum,
+          perferendis quas quia expedita.
+        </p>
+      </div>
+      <span className="text-gray-800">Copyright © 2025 UrpirioDev. Todos los derechos reservados.</span>
+    </footer>
+  );
 }
